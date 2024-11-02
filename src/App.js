f0846d6bb6a65055875dc577a957dc5603156385
NoOl01 <tagaevanton0@gmail.com>
2024-11-02 23:28:03 +0200

    Kanad router test 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './Test/Test';
+import Test2 from './TestPage2/TesePage2';
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
+        <header>
           <Link to="/">Главная</Link>
           <Link to="/about">О нас</Link>
-        </nav>
+          <Link to="/test2">Тест 2</Link>
+        </header>
         
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/test2" element={<Test2 />} />
         </Routes>
       </div>
     </Router>
--- src/Test/Test.js
+++ src/Test/Test.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import './Test.css'
 
 function About() {
   return (
     <div className='root'>
         <h1>Test</h1>
+        <Link to="/test2">Роут на тест 2</Link>
     </div>
   );
 }
