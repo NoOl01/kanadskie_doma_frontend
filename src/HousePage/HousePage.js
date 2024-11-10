de4d9e03819488d9444fc76ecfe9b460ef61a0f9
NoOl01 <tagaevanton0@gmail.com>
2024-11-11 00:24:57 +0200

    HousePage Created

--- src/HousePage/HousePage.js
+++ src/HousePage/HousePage.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './HousePage.css'
+import HouseProject from "./HouseProject";
 import {useParams} from "react-router-dom";
 
 function HousePage() {
+
     const { id } = useParams();
+    const [project, setProject] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/getHouseInfo?id=${id}/`, {
+            method: "GET"
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setProject(data)
+                console.log(data)
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
     return(
         <div className="house_page_root">
-            <p>tesdt</p>
+            {
+                project != null &&
+                <>
+                    <div className="projects_list">
+                        {project.data.houses.map(item => <HouseProject houseInfo={item}/>)}
+                    </div>
+                </>
+            }
         </div>
     )
 }
---
+++ src/HousePage/HouseProject.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './HouseProject.css'
+
+function HousePage({houseInfo}) {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default HousePage;
