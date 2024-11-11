aa3987800d67c0a6317761d743ef1a0be63aab05
Антон Тягаев <tyagaev-a@kbkit.pri>
2024-11-11 17:02:37 +0200

    House Page Slider created

--- src/HousePage/HouseProject.js
+++ src/HousePage/HouseProject.js
@@ -1,20 +1,47 @@
 import React from "react";
 import './HouseProject.css'
 import HouseSlider from "./Slider/HouseSlider";
+import {Autoplay, Pagination} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
 
 function HousePage({houseInfo}) {
     console.log(houseInfo);
-    return(
+    return (
         <div>
             <p>test</p>
             <p>{houseInfo.name}</p>
             <p>{houseInfo.house_code}</p>
             <p>{houseInfo.area}</p>
-            {
-                <>
-                    {houseInfo.images.map(images => <img src={images}/>)}
-                </>
-            }
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{
+                    clickable: true,
+                    el: '.custom-pagination', // Указываем элемент для пагинации
+                    renderBullet: (index, className) => {
+                        return `<span class="${className} custom-bullet"></span>`; // Настройка стилей для буллетов
+                    },
+                }}
+                loop={true}
+                modules={[Pagination, Autoplay]}
+                className="mySwiper1"
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}>
+
+                {
+                    <>
+                        {houseInfo.images.map(image => <SwiperSlide>
+                            <img src={image} alt=""/>
+                        </SwiperSlide>)}
+                    </>
+                }
+
+
+                {/* Элемент для пагинации */}
+                <div className="custom-pagination"></div>
+            </Swiper>
 
         </div>
     )
